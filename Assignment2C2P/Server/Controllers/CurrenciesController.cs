64baa00c84e0358f545b3913b9cc455fbaf8a7eb
Chatas <cchairin@manaosoftware.com>
2020-06-17 23:17:01 +0700

    Get currencies on transaction screen from controller

---
+++ Assignment2C2P/Server/Controllers/CurrenciesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Assignment2C2P.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrenciesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // TODO: Get data here
+
+            var result = new List<string> { "USD", "THB", "JPY" };
+
+            return Ok(result);
+        }
+    }
+}
