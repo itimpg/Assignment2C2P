d0cc15c00c587fe11a0b84220fe17f79f8d2fbdc
ManaoSoftware\cchairin <cchairin@manaosoftware.com>
2020-06-18 09:20:46 +0700

    Create IoC project

---
+++ Assignment2C2P.IoC/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Assignment2C2P.Business;
+using Assignment2C2P.Business.Interface;
+using Assignment2C2P.DataAccess.Repository;
+using Assignment2C2P.DataAccess.Repository.Interface;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Assignment2C2P.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection RegisterIoC(this IServiceCollection services)
+        {
+            services.AddTransient<ITransactionRepository, TransactionRepository>();
+            services.AddTransient<ITransactionManager, TransactionManager>();
+
+            return services;
+        }
+    }
+}
